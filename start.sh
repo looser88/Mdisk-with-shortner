caa7fbbf96870a8b031c3c4f2da740bc16f87f39
looser88 <98893135+looser88@users.noreply.github.com>
2023-04-23 13:49:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/looser88/Mdisk-with-shortner.git /Mdisk-with-shortner
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /Mdisk-with-shortner
 fi
 cd /URL-Shortener-V2
 pip3 install -U -r requirements.txt
